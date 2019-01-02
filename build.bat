685ebbd05b9cbd4f1286376a5ce8d718feb29cb6
Doğan Çelik <dogancelik93@gmail.com>
2019-01-02 03:40:09 +0300

    Refactor build script

--- build.bat
+++ build.bat
@@ -7,14 +7,22 @@ if "%1" == "zip" goto zip
 goto eof
 
 :build
-electron-packager . %npm_package_name% --platform=all --arch=x64 --version=%npm_package_electronVersion% --out=build --ignore="build"  --app-version="%npm_package_version%" --version-string.FileDescription="%npm_package_productName%" --version-string.ProductName="%npm_package_productName%" --icon=favicon.ico --overwrite
+echo ===============
+echo Package name: %npm_package_name%
+echo Electron version: %npm_package_electronVersion%
+echo App version: %npm_package_version%
+echo Product name: %npm_package_productName%
+echo ===============
+
+electron-packager ./ %npm_package_name% --platform=win32,linux,darwin --arch=x64 --electron-version=%npm_package_electronVersion% --no-tmpdir --out=../build --app-version="%npm_package_version%" --version-string.FileDescription="%npm_package_productName%" --version-string.ProductName="%npm_package_productName%" --icon=favicon.ico --overwrite
 
 goto eof
 
 :zip
 set ZIP_NAME=%~2
 set BUILD_NAME=%npm_package_name%-%ZIP_NAME%-x64
-set "BUILD_PATH=build/%BUILD_NAME%"
+set "BUILD_DIR=..\build\"
+set "BUILD_PATH=%BUILD_DIR%%BUILD_NAME%"
 
 echo ===============
 echo Zip name: %ZIP_NAME%
@@ -23,6 +31,7 @@ echo Build name: %BUILD_NAME%
 echo Build path: %BUILD_PATH%
 echo ===============
 
+mkdir %BUILD_DIR% >nul 2>&1
 cd %BUILD_PATH%
 
 echo Zip process started
