370aeafba102fecd201ccf49ca3595b8a530ac05
Doğan Çelik <dogancelik93@gmail.com>
2021-02-04 00:45:34 +0300

    Update from referenced file

--- shared.js
+++ shared.js
@@ -4,53 +4,76 @@ const electron = require('electron');
 const Menu = isRenderer ? electron.remote.Menu : electron.Menu;
 const MenuItem = isRenderer ? electron.remote.MenuItem : electron.MenuItem;
 
-exports.CustomMenu = function (template, model) {
-  var _menu = new Menu();
-  var _model = model;
+function updateMenuItems(m, model, tplArr = [], p) {
+  tplArr.forEach((tplItem) => {
+    if (!tplItem.menuItem) {
+      const menuItemOptions = {
+        click: tplItem.click,
+        role: tplItem.role,
+        type: tplItem.type,
+        label: tplItem.label,
+        sublabel: tplItem.sublabel,
+        accelerator: tplItem.accelerator,
+        icon: tplItem.icon,
+        id: tplItem.id,
+        position: tplItem.position,
+      };
 
-  function updateMenuItems (model, tplArr, p) {
-    tplArr.forEach(function (tplItem) {
-      if (!tplItem._item) {
-        tplItem._item = new MenuItem({
-          type: tplItem.type,
-          label: tplItem.label,
-          accelerator: tplItem.accelerator,
-          role: tplItem.role,
-          selector: tplItem.selector,
-          click: function (item, win) {
-            tplItem.click(_ret.getModel(), item, win)
-          },
-          submenu: Array.isArray(tplItem.submenu) ? new Menu() : null
-        })
-
-        p.append(tplItem._item)
+      if (Array.isArray(tplItem.submenu)) {
+        menuItemOptions.submenu = new Menu();
+      }
+      if (tplItem.click) {
+        menuItemOptions.click = (item, win) => {
+          tplItem.click(m.getModel(), item, win);
+        };
       }
 
-      if (typeof tplItem.visible === 'function') {
-        tplItem._item.visible = tplItem.visible(model, tplItem._item)
-      }
+      // eslint-disable-next-line no-param-reassign
+      tplItem.menuItem = new MenuItem(menuItemOptions);
 
-      if (typeof tplItem.enabled === 'function') {
-        tplItem._item.enabled = tplItem.enabled(model, tplItem._item)
-      }
-
-      if (tplItem._item.submenu) {
-        updateMenuItems(model, tplItem.submenu, tplItem._item.submenu)
-      }
-    })
-
-    return p
-  }
-
-  var _ret = {
-    getMenu: function () { return _menu; },
-    getModel: function () { return _model; },
-    update: function (model) {
-      _model = model
-      updateMenuItems(_ret.getModel(), template, _ret.getMenu())
+      p.append(tplItem.menuItem);
     }
+
+    if (typeof tplItem.visible === 'function') {
+      // eslint-disable-next-line no-param-reassign
+      tplItem.menuItem.visible = tplItem.visible(model, tplItem.menuItem);
+    }
+
+    if (typeof tplItem.enabled === 'function') {
+      // eslint-disable-next-line no-param-reassign
+      tplItem.menuItem.enabled = tplItem.enabled(model, tplItem.menuItem);
+    }
+
+    if (tplItem.menuItem.submenu) {
+      updateMenuItems(m, model, tplItem.submenu, tplItem.menuItem.submenu);
+    }
+  });
+
+  return p;
+}
+
+function createMenu(template, initialModel) {
+  const menu = new Menu();
+  let localModel = initialModel;
+
+  const m = {
+    getMenu() {
+      return menu;
+    },
+
+    getModel() {
+      return localModel;
+    },
+
+    update(newModel) {
+      localModel = newModel;
+      updateMenuItems(this, m.getModel(), template, m.getMenu());
+    },
   };
 
-  updateMenuItems(_ret.getModel(), template, _ret.getMenu())
-  return _ret;
-};
+  updateMenuItems(m, m.getModel(), template, m.getMenu());
+
+  return m;
+}
+
+exports.CustomMenu = createMenu;
