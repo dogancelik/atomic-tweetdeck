2c12f78bcac47eb35dcf0566cc8cef16c0034f97
Doğan Çelik <dogancelik93@gmail.com>
2021-05-02 08:10:31 +0300

    Add toggle fullscreen

--- typings/atomic-tweetdeck/index.d.ts
+++ typings/atomic-tweetdeck/index.d.ts
@@ -1,4 +1,22 @@
 declare namespace AtomicTweetdeck {
-    type MenuTemplate = Array<(Electron.MenuItemConstructorOptions)>;
-}
+	type MenuTemplate = Array<(Electron.MenuItemConstructorOptions)>;
 
+	class BrowserWindowUndoc extends Electron.BrowserWindow {
+		browserWindowOptions: Electron.BrowserWindowConstructorOptions
+	}
+
+	interface NewWindowExtendedEvent extends Electron.NewWindowWebContentsEvent {
+		sender: BrowserWindowUndoc
+	}
+
+	type NewWindowEventListener = (event: NewWindowExtendedEvent,
+		url: string,
+		frameName: string,
+		disposition: string,
+		options: Electron.BrowserWindowConstructorOptions,
+		additionalFeatures: string[],
+		referrer: Electron.Referrer,
+		postBody: Electron.PostBody) => void;
+
+	type NewWindowEventListenerWrapper = (parentWindow: Electron.BrowserWindow) => NewWindowEventListener;
+}
